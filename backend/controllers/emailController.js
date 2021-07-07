dcfc0c243c7482d33739e095fbe34d0025e87be2
Abdullah0322 <abdullahnaveed71.am@gmail.com>
2021-07-07 15:08:50 +0500

    push

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -3,12 +3,23 @@ import mailer from 'nodemailer'
 import ejs from "ejs"
 import htmlToText from 'html-to-text';
 
-
+import {google} from 'googleapis'
 
 import path from 'path'
 
 const __dirname = path.resolve()
 
+const CLIENT_ID = '807669913381-mk2cci6qqnkv9gjn4d6a2s41jq079t1t.apps.googleusercontent.com';
+const CLEINT_SECRET = 'd0c0YQ3wesgJltUUKbmcpuCy';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04ilEdhBFvk56CgYIARAAGAQSNwF-L9Ir0orcAqsXUXvauuKwCPQlMku9aEh7reDDLksPqEHI46AfaTbly3WBQhRjqVtzut5hAZc';
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLEINT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 // const oAuth2Client = new google.auth.OAuth2(
 //   process.env.CLIENT_ID,
 //   process.env.CLEINT_SECRET,
@@ -26,6 +37,7 @@ const getEmailData = (to, name, cc, ticketData) => new Promise(async(resolve, re
     const html= await ejs.renderFile(`${__dirname}/views/email.ejs`, {
       ticketData
   })
+
     
     let data = null;
     console.log('to: ', to);
@@ -47,14 +59,19 @@ const getEmailData = (to, name, cc, ticketData) => new Promise(async(resolve, re
 
 const sendEmail = async(to, name, cc, ticketData) => {
   
-    
+  const accessToken = await oAuth2Client.getAccessToken();
+
   const smtpTransport = mailer.createTransport({
-    host:"smtp.gmail.com",
-    port: 587,
+    service: 'gmail',
+
     auth: {
-      user: "abdullahnaveed71.am@gmail.com",
-     pass:"malikdulli12"
-    }
+      type: 'OAuth2',
+      user: 'abdullah.naveed@gigalabs.co',
+      clientId: CLIENT_ID,
+      clientSecret: CLEINT_SECRET,
+      refreshToken: REFRESH_TOKEN,
+      accessToken: accessToken,
+    },
   });
   
   // console.log('name: ', name);

d37785f34adb1e12bdd46683664792644e2c3e1c
Abdullah0322 <abdullahnaveed71.am@gmail.com>
2021-07-18 21:49:25 +0500

    ok

--- frontend/src/globals.js
+++ frontend/src/globals.js
@@ -1,6 +1,8 @@
-const SERVER= "http://localhost:5000"
+const SERVER= "https://ticketupdater.herokuapp.com"
 
 export default SERVER
 
 //http://localhost:5000
-//
+
+
+//https://ticketupdater.herokuapp.com/
